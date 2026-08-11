6960ba34a0723f0594315481adcaceeb855cde5b
Anos <bebem4@bebes-MacBook-Air.local>
2026-08-11 21:17:12 +0300

    /

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/basketball_screen.dart';
+import 'package:test_project/screens/basketball_screen/basketball_screen.dart';
 
 void main() {
   runApp(const MyApp());
